0995dfd28e29d515c5ac53dbfbf75d11a71bda81
sahil <sahilraj806@gmail.com>
2026-08-05 15:18:43 +0530

    constant file update

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'APP_ASSET_MODE' => 'dev', // Options: 'dev' or 'live'
+    'APP_ASSET_MODE' => isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='admin.oneearthholidays.com' ? 'live' : 'dev' // Options: 'dev' or 'live'
 ];
